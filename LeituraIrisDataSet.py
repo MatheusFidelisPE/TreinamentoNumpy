aeec51d511a2240a2f202a8676a8163fb3657123
Matheus Fidelis da Silva <mfds@a.recife.ifpe.edu.br>
2020-11-25 10:13:58 -0300

    manipulando dataset iris para calcular média, mediana, variancia, desvio padrão

--- LeituraIrisDataSet.py
+++ LeituraIrisDataSet.py
@@ -1,7 +1,8 @@
 import numpy as np
 
+listaAtributos = []
 def valor_classe(data):
-
+    global listaAtributos
     for i in range(150):
         if (data[i, 4] == 'Iris-setosa'):
             data[i, 4] = 1
@@ -9,11 +10,13 @@ def valor_classe(data):
             data[i, 4] = 2
         else:
             data[i, 4] = 3
-
     data = data.astype('float')
     return data
 
 data = np.genfromtxt("iris.data",delimiter=',',dtype= str)
+data1 = np.genfromtxt("iris.data",delimiter=',',dtype= str)
+
+
 print(data.shape)
 data = valor_classe(data)
 print(data)
@@ -23,10 +26,24 @@ print("Mínimo valor: ", data[:,0].min())
 print("Máximo valor: ", data[:,0].max())
 
 Iris_virginica = data[:,4] == 1
-print(data[Iris_virginica])
-print("Mínimo valor: ", data[Iris_virginica,0].min())
+#print(data[Iris_virginica])
+'''print("Mínimo valor: ", data[Iris_virginica,0].min())
 print("Máximo valor: ", data[Iris_virginica,0].max())
 print('Média: ', data[Iris_virginica,0].mean())
 print('Variancia: ', np.var(data[Iris_virginica,0]))
 print('Mediana: ', np.median(data[Iris_virginica,0]))
 print('Desvio Padrão: ', np.std(data[Iris_virginica,0]))
+'''
+print()
+for i in range(4):
+    print('COLUNA: ', i)
+    print('Média dos valores: ',i ,np.mean(data[:,i:i+1]))
+    print('Mediana: ', np.median(data[:,i]))
+    print('Mínimo: ', data[:, i].min())
+    print('Máximo: ', data[:,i].max())
+    print('Vâriancia: ', np.var(data[:,i]))
+    print('Desvio Padrão: ', np.std(data[:,i]))
+    print('Desvio padrão calculado usando variancia: ', np.sqrt(np.var(data[:,i])))
+    print()
+
+print(np.sqrt(10))
